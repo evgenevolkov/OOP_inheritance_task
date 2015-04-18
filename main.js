3dbac0e854d35050932f9beda06bd5e71cb862a1
Evgene Volkov <evolkov.git@gmail.com>
2015-04-19 02:28:03 +0300

    Task completed.
    Inheritannce is working properly.
    Strategy pattern is working properly.

--- main.js
+++ main.js
@@ -3,100 +3,122 @@
  */
 
 /// At first I'm creating general BeingClass. It will be the top of all being classes.
-function BeingClass(name) {
-    this.legs = 4;
-    this.name = name;
-    return this.name;
+function BeingClass(clas) {
+    this.clas = clas;
+    this.movementType = run;
 }
 
 // Than I'm adding move function to all beings.
-BeingClass.prototype.move = function () {
-    console.log(this.name + " is running")
+BeingClass.prototype.makeMove = function () {
+    return this.movementType.move(this.name);
 };
 
-//Animal.prototype.setMovementType = function (movementType) {
-//    this.move = movementType;
+//Making a method to print object's properties
+BeingClass.prototype.logName = function () {
+    console.log("This.class: " + this.clas + "; this.name: " + this.name + "; this.legs= " + this.legs)
+};
+
+//setting movement methods, strategy and type switch - BEGIN
+BeingClass.prototype.setMovementType = function (newMovementType) {
+    this.movementType = newMovementType;
+};
+
+var Movement = function (func) {
+    this.move = func;
+};
+
+var jump = new Movement(function (name) {
+    console.log(name + " is jumping!")
+});
+
+var run = new Movement(function (name) {
+    console.log(name + " is running!");
+});
+var fly = new Movement(function (name) {
+    console.log(name + " is flying!")
+});
+
+//setting movement methods, strategy and type switch - END
+
+
+// ---- please skip these comments
+/// setting speak methods and strategy
+//BeingClass.prototype.speak = function() {
+//    this.speak()
 //};
-//Animal.prototype.move = function (movementType) {
-//     run();
-//};
-//var run = function () {
-//    console.log (this.name + " is running")
-//};
-//
-//animal1.setMovementType(run);
+// -----
+
 
 // Now I'm creating animal class. It will be used for all animals.
-var animalClass = new BeingClass("animalClass");
-
-// adding speak method that will say it does't speak.
-animalClass.speak = function () {
-    cosole.log(this.name + "is an animal. It can't speak")
+//
+var AnimalClass = function (clas, name, legs, movementType) {
+    BeingClass.call(this, clas);
+    this.legs = legs;
+    this.name = name;
+    this.setMovementType(movementType)
 };
 
-// Now creating first animal - rabbit
-//var rabbit = {};
-var rabbit = Object.create(animalClass);
-rabbit.name = "rabbit";
+AnimalClass.prototype = Object.create(BeingClass.prototype);
 
+//// adding speak method that will say it does't speak.
+//animalClass.speak = function () {
+//    cosole.log(this.name + "is an animal. It can't speak")
+//};
+
+// Now creating first animal - rabbit
+var rabbit = new AnimalClass("Rabbit class", "Rabbit Tim", 4, jump);
 
 // Checking out animal class functions and properties
-console.log("---Now checking " + animalClass.name);
-console.log("AnimalClass.name: " + animalClass.name);
-console.log("AnimalClass is: " + animalClass);
-console.log("AnimalClass constructor is: " + animalClass.constructor);
-console.log("AnimalClass legs amount is: " + animalClass.legs);
-animalClass.move();
+//console.log("---Now checking " + AnimalClass.name);
+//console.log("AnimalClass is: " + AnimalClass);
+//console.log("AnimalClass constructor is: " + AnimalClass.constructor);
+//console.log("AnimalClass legs amount is: " + AnimalClass.legs);
+//AnimalClass.move();
 
 // Checking out rabbit functions and properties
-console.log("---Now checking " + rabbit.name);
-console.log("rabbit.name: " + rabbit.name);
-console.log("rabbit is: " + rabbit);
-console.log("rabbit constructor is: " + rabbit.constructor);
-console.log("rabbits legs amount is: " + rabbit.legs);
-rabbit.move();
+console.log("-  Logging out rabbit: ");
+rabbit.logName();
+rabbit.makeMove();
 
 // creating another rabbit - babyRabbit. It will inherit from 'rabbit'
 //var rabbitBaby = {};
 
-// Now creating another one rabit
-var rabbitBaby = Object.create(rabbit);
+// Now creating bird
+var bird = new AnimalClass("Bird class", "Birdie", 0, fly);
+bird.logName();
+bird.makeMove();
 
-console.log("rabitBaby is currently a baby, so it's name like it's parent. ");
-console.log("rabbitBaby.name: " + rabbitBaby.name);
-console.log("rabbitBaby is: " + rabbitBaby);
-console.log("rabbitBaby's legs amount is: " + rabbitBaby.legs);
-rabbitBaby.move();
-
-// Now changing rabitBaby's name.
-rabbitBaby.name = "rabbitBaby";
-console.log("rabbitBaby grew up. It has it's own name now");
-console.log("now rabbitBaby.name: " + rabbitBaby.name);
-
-// In order to check inheritance I'm changing rabbit.property
+// In order to check the work of inheritance I'm changing rabbit.property
 console.log("---Rabbits population suddenly mutated. All rabbits legs amount decreased to 3...");
 rabbit.legs = 3;
 console.log("rabbit's legs amount now is: " + rabbit.legs);
-console.log("rabbitBaby's legs amount now is: " + rabbitBaby.legs);
 
 // now creating Human being class. It will be used as specimen for all new human being creation
 console.log("---Now creating human");
 
-var HumanBeingClass = new BeingClass();
-
-// Changing number of legs
-HumanBeingClass.legs = 2;
+var HumanBeingClass = function (name, familyName, movementType) {
+    BeingClass.call(this, "HumanBeing class");
+    this.legs = 2;
+//    this.class = "AnimalClass";
+    this.name = name;
+    this.setMovementType(movementType)
+};
+HumanBeingClass.prototype = Object.create(BeingClass.prototype);
 
 //Now creating first human being.
-//var humanJack = {};
-var humanJack = Object.create(HumanBeingClass);
-humanJack.name = "Jack";
+var humanJack = new HumanBeingClass("Jack", "Johnson", run);
 
 //Checking out Jack's properties and functionality
 console.log("Jack.name: " + humanJack.name);
-console.log("Jack is: " + humanJack.name);
 console.log("Jack constructor is: " + humanJack.constructor);
 console.log("Jack legs amount is: " + humanJack.legs);
-humanJack.move();
+
+humanJack.logName();
+humanJack.makeMove();
+
+// Change Jack's movement type to check if STRATEGY PATTERN works in a proper way
+console.log("Jack has just learned to jump");
+humanJack.setMovementType(jump);
+humanJack.makeMove();
 
 console.log("THE END");
